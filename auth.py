e2e120e3dfc2f1a64f6204a7eb0d8c5a08e526a0
katalinschmidt <katalin.schmidt.97@gmail.com>
2021-10-11 09:24:21 -0700

    Undo second Flask Blueprint and finish basic API-related functions
    Realized there was really no need for second Blueprint since API-related functions can simply be imported into the views.py file which will keeps things simpler and cleaner. Added a bit of logic for testing user access dependent on the session and a new HTML page for when the user passes 'login'.

--- auth.py
+++ auth.py
@@ -1,45 +1,83 @@
+"""This file contains the methods for handling low-level access to the Strava API"""
+
 # 'flask' is the micro web app framework, from which you can import useful classes and functions
 from flask import Blueprint, redirect, Response, request, session 
 # 'urllib.parse' is a module that provides functions for manipulating URLs
 import urllib.parse
+# 'requests' is a library that simplifies how you send HTTP requests
+import requests
 # 'os' allows you to interact with your underlying operating system, e.g. access environmental variables
 import os
 
-# Blueprints allow you to break up your application into modules and thereby help organize views / code.
-# Create an instance of the class Blueprint:
-auth = Blueprint('auth', __name__)
 
 # Creating environmental variables allows us to hide sensitive data:
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 REDIRECT_URI = os.environ['REDIRECT_URI']
 
+# Global variables are created outside of functions, so that they can be accessed everywhere:
+# Constant variables are used to hold information that will not be changed later:
 STRAVA_AUTH_URL = "https://www.strava.com/oauth/authorize"
 STRAVA_TOKEN_URL = "https://www.strava.com/oauth/token"
 API_BASE_URL = "https://www.strava.com/api/v3/athlete/activities"
 
 
-# Use the route decorator to connect your view / function to your desired URL:
-@auth.route('/strava_login')
-def strava_login():
-    """Prompt user to 'login' with Strava profile"""
+def prompt_strava_login():
+        """Prompt user to authorize connection to Strava"""
 
-    params = {
-        'client_id': CLIENT_ID,
-        'response_type': 'code',
-        'redirect_uri': REDIRECT_URI,
-        'approval_prompt': 'force',
-        'scope': 'activity:read_all'
-        }
+        params = {
+            'client_id': CLIENT_ID,
+            'response_type': 'code',
+            'redirect_uri': REDIRECT_URI,
+            'approval_prompt': 'force',
+            'scope': 'activity:read_all'
+            }
 
-    return redirect("{}?{}".format(
-        STRAVA_AUTH_URL,
-        urllib.parse.urlencode(params)
-    )) 
+        return redirect("{}?{}".format(
+            STRAVA_AUTH_URL,
+            urllib.parse.urlencode(params)
+        )) 
 
 
-@auth.route('/strava_oauth')
-def strava_oauth():
-    """Get access to & make call to STRAVA API"""
+def get_oauth_code():
+    """Parse oauth code from URL for access to STRAVA API"""
 
-    return "<h1>TEST OAUTH</h1>"
+    OAUTH_CODE = request.args.get('code', None)
+
+    if not OAUTH_CODE:
+        return Response('Error: Missing authorization', status=400)
+    
+    return OAUTH_CODE
+
+
+def exchange_tokens(OAUTH_CODE):
+    """Exchange oauth code for access tokens"""
+
+    data = {
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'code': OAUTH_CODE,
+            'grant_type': 'authorization_code'
+            }
+
+    # TOKENS = requests.post(STRAVA_TOKEN_URL, data=data).json()
+    TOKENS = requests.post(url=STRAVA_TOKEN_URL, data=data).json()
+
+    return TOKENS
+
+
+def save_tokens(TOKENS):
+    """Save tokens to session & database"""
+
+    session['athlete_id'] = TOKENS['athlete']['id']
+    session['access_token'] = TOKENS['access_token']
+    session['refresh_token'] = TOKENS['refresh_token']
+    session['expires_at'] = TOKENS['expires_at']
+
+    # if not crud.get_athlete(TOKENS['athlete']['id']):
+    #     crud.create_athlete(
+    #                         TOKENS['athlete']['id'],
+    #                         TOKENS['athlete']['firstname'],
+    #                         TOKENS['athlete']['lastname'],
+    #                         TOKENS['athlete']['profile'],
+    #                        ) 
--- views.py
+++ views.py
@@ -1,7 +1,9 @@
 """Create Flask web app routes to render HTML pages of web app"""
 
-# 'flask' is the micro web app framework, 'Blueprint' is a class, and 'render_template' is a function
-from flask import Blueprint, render_template
+# 'flask' is the micro web app framework, from which you can import useful classes and functions
+from flask import Blueprint, session, render_template
+# 'auth' is a file containing self-made methods to handle API connection
+import auth
 
 # Blueprints allow you to break up your application into modules and thereby help organize views / code.
 # Create an instance of the class Blueprint:
@@ -14,3 +16,29 @@ def show_homepage():
     """Show homepage"""
 
     return render_template("homepage.html")
+
+
+@views.route('/login')
+def user_login():
+    """Check if user is in session & handle login accordingly"""
+    
+    # FIXME: Next line is for testing only! Delete after testing is complete
+    # session['access_token'] = None'
+
+    if session.get('access_token', None):
+        return render_template("activities.html")       
+    
+    else:
+        print("No access_token in session") # FIXME: Delete this line if testing complete
+        return auth.prompt_strava_login()
+
+
+@views.route('/strava_oauth')
+def connect_to_api():
+    """Connect to Strava API"""
+
+    OAUTH_CODE = auth.get_oauth_code()
+    TOKENS = auth.exchange_tokens(OAUTH_CODE)
+    auth.save_tokens(TOKENS)
+
+    return render_template("activities.html")
