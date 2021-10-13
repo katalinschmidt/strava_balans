163cf48134da137bcdf76056802503b02055c8e8
katalinschmidt <katalin.schmidt.97@gmail.com>
2021-10-13 11:54:03 -0700

    Decode & render polylines on Leaflet map

--- static/leaflet.js
+++ static/leaflet.js
@@ -15,9 +15,12 @@ console.log("Connected to leaflet.js!")
 // console.log(activity) // OUTPUT = Good! NodeList of activity names
 // console.log(polyline) // OUTPUT = Good! NodeList of polylines
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+import polyline from './leaflet_util.js';
 
-renderLealet()
+// Call function render map:
+renderLealet();
 
+// Define function to render map:
 function renderLealet() {
     console.log("Executing function renderLeaflet...");
 
@@ -35,4 +38,23 @@ function renderLealet() {
     // Add tiles to map:
     tiles.addTo(map);
 
+    // Iterate through polylines & decode:
+    const all_polylines = document.querySelectorAll('.activity-polyline');
+    for (let activity_line of all_polylines) {   
+        // Get innerHTML & strip whitespace:
+        activity_line = activity_line.innerHTML.trim()
+        
+        // 'polyline.decode' is a helper function from leaflet_util.js (edited for accurate lat/long order)
+        // Req: polyline string, percision num. Returns: array of lat/long coordinates
+        activity_line = polyline.decode(activity_line, 5);
+
+        // Add lat/longs for each activity to map:
+        L.polyline(
+            activity_line,
+            {color: "red",
+            weight: 4,
+            opacity: .7,
+            lineJoin: 'round'}
+        ).addTo(map);
+    }
 }
